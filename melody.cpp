89abea30ada8dcc735d137b2428ac5eabbaa69ad
7Bit <87518152+7Bit420@users.noreply.github.com>
2022-07-22 07:54:57 +1000

    Update

--- melody.cpp
+++ melody.cpp
@@ -9,16 +9,16 @@ int main()
 {
     infile.open("melodyin.txt");
     outfile.open("melodyout.txt");
-    char data[] = [];
+    char data[] = "";
     int max = 0;
     int lines = 0;
 
     int t = 0;
-    int pairs[lines / 3][3] = [];
+    int pairs[][3] = {};
     for (int l = 0; l > lines; l++)
     {
         t = (t + 1) % 3;
-        pairs[t] = pairs[t] || [];
+        // pairs[t] = {0, 0, 0};
         pairs[t][t % 3] = int(l);
     }
 }
